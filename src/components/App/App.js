fef0d21601dbc5b067abd141bcc4f2e3245a8092
ArthurMur <archeeandrews@yandex.ru>
2023-10-29 20:30:53 +0300

    fix: поменял эффект для фильтрации

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -404,7 +404,7 @@ const handleLogin = async ({ email, password }) => {
     } else {
       setSavedFilteredInitialMovies(savedInitialMovies);
     }
-  }, [savedInitialMovies]);
+  }, [savedInitialMovies, savedFilteredInitialMovies]);
   // useEffect(() => {
   //   // Проверяем, сохранена ли информация о фильтрации в локальном хранилище
   //   const isFilterChecked = localStorage.getItem('checked-save') === 'true';
